e328ec6567c694ed2f87c9349ad790e57afb434b
owlgirl07 <jessica.alyssa.07@live.ca>
2019-05-02 16:41:24 -0300

    created Balloon class

--- FormFrontEnd/Form1.cs
+++ FormFrontEnd/Form1.cs
@@ -11,7 +11,10 @@ using System.Windows.Forms;
 namespace FormFrontEnd
 {
     public partial class Form1 : Form
-    {     
+    {
+
+        Balloon myFirstBalloon;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,36 @@ namespace FormFrontEnd
 
         private void btnSet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                //I must instantiate my object
+                myFirstBalloon = new Balloon() {
+
+                    Colour = txtColor.Text.Trim(),
+                    Height = Convert.ToDecimal(txtHeight.Text),
+                    Diameter = Convert.ToDecimal(txtDiameter.Text)
+
+                };
+
+                //myFirstBalloon.Colour = txtColor.Text.Trim();.Text);
+                //myFirstBalloon.Height = Convert.ToDecimal(txtHeight.Text);
+                //myFirstBalloon.Diameter = Convert.ToDecimal(txtDiameter.Text);
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            
+            lblOutput.Text = $"Colour: {myFirstBalloon.Colour}\n" +
+                $"Height: {myFirstBalloon.Height.ToString()}\n" +
+                $"Diameter: {myFirstBalloon.Diameter.ToString()}";
         }
     }
 }
